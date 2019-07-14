b64b7531b316d10c816e18088a38cc7603094bb6
ADMIN <dotoan.haui@gmail.com>
2019-07-14 22:20:47 +0700

    btvn

---
+++ authen/resources/views/admin/partials/footer.blade.php
@@ -0,0 +1,3 @@
+<div class="footer">
+    <p>&copy; 2018 Glance Design Dashboard. All Rights Reserved | Design by <a href="https://www.facebook.com/nguyen.dotoan.7" target="_blank">Nguyễn Đỗ Toàn</a></p>
+</div>
